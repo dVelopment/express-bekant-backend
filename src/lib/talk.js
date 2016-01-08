4fd7e838ebef58dcf693f6e4502f4beab929be88
Daniel Holzmann <d@velopment.at>
2016-01-08 18:35:34 +0100

    use predefined files for audio output

--- src/lib/talk.js
+++ src/lib/talk.js
@@ -2,46 +2,29 @@
 
 import request from 'request';
 import {spawn} from 'child_process';
-import temp from 'temp';
 import fs from 'fs';
-temp.track();
+import settings from './settings';
+import path from 'path';
+
+let dir = settings.get('talk').audioDir;
 
 function talk(position, language = 'de', cb) {
     if ('function' === typeof language) {
         cb = language;
         language = 'de';
     }
-    let options = {
-        url: 'https://translate.google.com/translate_tts',
-        qs: {
-            ie: 'UTF-8',
-            q: position.label,
-            tl: language,
-            total: 1,
-            idx: 0,
-            textlen: position.label.length,
-            tk: '162937|286006',
-            client: 't'
-        },
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
-            'Referer': 'https://translate.google.com/'
-        }
-    };
 
-    temp.open({suffix: '.mp3'}, (err, info) => {
-        if (err) {
-            throw err;
-        }
-        request(options).pipe(fs.createWriteStream(info.path))
-            .on('close', () => {
-                let proc = spawn('mpg123', [info.path]);
-                proc.on('close', function () {
-                    temp.cleanup();
-                    cb();
-                });
-            });
-    });
+    let audioFile = path.join(dir, position.label + '.mp3');
+
+    let stat = fs.statSync(audioFile);
+    if (stat.isFile() && stat.size) {
+        let proc = spawn('mpg123', [audioFile]);
+        proc.on('close', function () {
+            cb();
+        });
+    } else {
+        cb();
+    }
 }
 
 export default talk;
